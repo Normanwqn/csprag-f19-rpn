64bcb665f788bc2393825a5b8c8d860f6a0a066b
normanqw <normanqw@umich.edu>
2019-10-25 14:35:10 -0400

    Implemented Addition (add function)

--- rpn.py
+++ rpn.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python3
 def calculate(arg):
-    pass
-
+    stack = list()  # create the stack
+    for token in arg.split():
+        print(token)
+        if token == '+':
+            arg1 = stack.pop()
+            arg2 = stack.pop()
+            result = arg1 + arg2
+            stack.append(result)
+        else:
+            stack.append(int(token))
+        print(stack)
+    return stack.pop()
 def main():
     while True:
         calculate(input("rpn calc> "))
+
 
 if __name__ == "__main__":
     main()
